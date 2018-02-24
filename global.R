ea7723252f141427d560432ddb72b55e9a186e2c
paihui <34162955+paihui@users.noreply.github.com>
2018-02-24 16:26:09 +0800

    Add files via upload

---
+++ global.R
@@ -0,0 +1,3 @@
+setwd("C:/BigDataSpark/R")
+
+source("部會Demo2.R")
---
+++ shiny.R
@@ -0,0 +1,17 @@
+#============================================================
+# Demo3: 互動式網頁
+#============================================================
+
+
+#設定工作路徑
+setwd("C:/BigDataSpark/R")
+
+#引用 shiny
+#install.packages("shiny")
+library(shiny)
+
+#執行範例網頁
+#runExample("01_hello")
+
+#執行
+runApp("WordCloud-App")
